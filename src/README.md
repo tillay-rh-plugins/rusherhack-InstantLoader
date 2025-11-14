f668a01b1a30da16e82d72a5501cf748a3fd24e2
tillay8 <tilleyy8@gmail.com>
2025-11-14 00:17:49 -0700

    add rendering, cicd, and readme

---
+++ src/README.md
@@ -0,0 +1,18 @@
+# InstantLoader plugin
+
+### Automatically interacts with a targeted trapdoor as soon as a specified player logs in, enabling instant pearl loading.
+___
+This plugin is designed to be able to use an alt account to instantly pull a pre-set pearl and get another account out of a logout trap or sticky situation.
+___
+### How to use:
+
+Note: This plugin requires two instances of minecraft open. One that is your alt account, and another that is the account that needs rescuing.
+
+1. Enable the module on your alt's instance of minecraft. Change the username field to be the username of the player you want to rescue. Case does not matter. 
+2. With your alt, log in and target the trapdoor of the pearl stasis chamber. It should light up with a green box after you hover your crosshair over it.
+3. With your primary account, log in. Your alt should automatically interact with the trapdoor, pulling the pearl. 
+4. Enjoy not having been exploded and take a deep breath to calm your nerves.
+___
+When you hover over the trapdoor, it should have a box rendered on it if the module is on. If the box is red, that means there is no pearl under the trapdoor, and if the box is blue, that means the trapdoor is closed. 
+
+If the box is green, then it should be good to go and you can log in with your main account. 
--- src/main/java/org/tilley/InstantLoadModule.java
+++ src/main/java/org/tilley/InstantLoadModule.java
@@ -1,40 +1,67 @@
 package org.tilley;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.protocol.game.ClientboundPlayerInfoUpdatePacket;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.projectile.ThrownEnderpearl;
+import net.minecraft.world.level.block.TrapDoorBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.BlockHitResult;
 import org.rusherhack.client.api.events.network.EventPacket;
+import org.rusherhack.client.api.events.render.EventRender3D;
 import org.rusherhack.client.api.feature.module.ModuleCategory;
 import org.rusherhack.client.api.feature.module.ToggleableModule;
+import org.rusherhack.client.api.render.IRenderer3D;
 import org.rusherhack.client.api.utils.ChatUtils;
 import org.rusherhack.core.setting.StringSetting;
 import org.rusherhack.core.event.subscribe.Subscribe;
 
 public class InstantLoadModule extends ToggleableModule {
-	StringSetting username = new StringSetting("Usernames", "tilley8");
+	StringSetting username = new StringSetting("Username", mc.getUser().getName().toLowerCase());
 
 	public InstantLoadModule() {
 		super("InstantLoad", "Load a trapdoor as soon as the target logs on", ModuleCategory.COMBAT);
-
 		this.registerSettings(username);
 	}
 
     @Subscribe
     public void onPacketReceive(EventPacket.Receive event) {
         if (!(event.getPacket() instanceof ClientboundPlayerInfoUpdatePacket packet)) return;
+        if (packet.newEntries().stream().noneMatch(p -> p.profile() != null &&
+            p.profile().getName().equalsIgnoreCase(username.getValue()))) return;
 
-        boolean trigger = packet.newEntries().stream().anyMatch(
-                p -> p.profile() != null && p.profile().getName().equalsIgnoreCase(username.getValue())
-        );
-        if (!trigger) return;
-
-        ChatUtils.print(username.getValue() + " has joined! Loading pearl...");
-        interactTargetedBlock();
-    }
-
-	private void interactTargetedBlock() {
-		if (!(mc.hitResult instanceof BlockHitResult bhr) || mc.gameMode == null) return;
+        ChatUtils.print(username.getValue() + " has joined!");
+        if (!(mc.hitResult instanceof BlockHitResult bhr) || mc.gameMode == null || mc.level == null ||
+            !(mc.level.getBlockState(bhr.getBlockPos()).getBlock() instanceof TrapDoorBlock)) {
+            ChatUtils.print("Unable to load pearl! No trapdoor was targeted.");
+            return;
+        }
 
         mc.execute(() -> mc.gameMode.useItemOn(mc.player, InteractionHand.MAIN_HAND, bhr));
-	}
+        ChatUtils.print("Loaded pearl!");
+    }
+
+    @Subscribe
+    private void onRender3D(EventRender3D event) {
+        if (!(mc.hitResult instanceof BlockHitResult bhr) || mc.gameMode == null || mc.level == null ||
+            !(mc.level.getBlockState(bhr.getBlockPos()).getBlock() instanceof TrapDoorBlock)) return;
+
+        BlockPos pos = bhr.getBlockPos();
+        BlockState state = mc.level.getBlockState(pos);
+
+        if (state.getBlock() instanceof TrapDoorBlock) {
+            BlockState closedState = state.setValue(TrapDoorBlock.OPEN, false);
+            AABB trapdoorAABB = closedState.getCollisionShape(mc.level, pos).bounds().move(pos);
+            boolean hasPearl = !mc.level.getEntitiesOfClass(ThrownEnderpearl.class, trapdoorAABB).isEmpty();
+            int color = !state.getValue(TrapDoorBlock.OPEN) ? 0xFF0000FF : hasPearl ? 0xFF00FF00 : 0xFFFF0000;
+
+            IRenderer3D renderer = event.getRenderer();
+            renderer.begin(event.getMatrixStack());
+            renderer.setLineWidth(5f);
+            renderer.drawBox(pos, false, true, color);
+            renderer.end();
+        }
+    }
+
 }
